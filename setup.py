af897310bd382fad06adc5267961e4fd2330d323
Kishore Angani <kishore@smarthive.ai>
2019-09-11 14:36:06 +0530

    BW-97: version changes

--- setup.py
+++ setup.py
@@ -1,30 +1,20 @@
 #!/usr/bin/env python
-# coding=utf-8
 
 from setuptools import setup
 
 package_name = 'smarthive'
 filename = package_name + '.py'
 
-def get_version():
-    import ast
-    with open(filename) as input_file:
-        for line in input_file:
-            if line.startswith('__version__'):
-                return ast.parse(line).body[0].value.s
-
-
 def get_long_description():
     try:
         with open('README.md', 'r') as f:
-            return f.read()
+            return f.read();
     except IOError:
         return ''
 
-
-setup(
+setup (
     name=package_name,
-    version=get_version(),
+    version='0.0.1',
     author='SmartHive Automation',
     author_email='dev@smarthive.ai',
     description='SmartHive Cloud Controller',
