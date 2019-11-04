2eaa461b6b1c45388eb3b03f4fff4ea99010b686
Kishore Angani <kishore@smarthive.ai>
2019-11-04 15:52:57 +0530

    releasing 0.0.7 with zeroconf resulution for mdns

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_long_description():
 
 setup (
     name=package_name,
-    version='0.0.6',
+    version='0.0.7',
     author='SmartHive Automation',
     author_email='dev@smarthive.ai',
     description='SmartHive Cloud Controller',
--- smarthive.py
+++ smarthive.py
@@ -8,10 +8,10 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import socket
 import threading
 import logging
-from zeroconf import ServiceInfo, Zeroconf
+from zeroconf import ServiceInfo, ServiceBrowser, Zeroconf
 import http.client
 import uuid
- 
+
 localHTTP = None
 zeroconf = None
 info = None
@@ -19,6 +19,7 @@ gwconnection = None
 
 SNAP_COMMON = os.environ['SNAP_COMMON']
 HOST = "a1x9b1ncwys18b-ats.iot.ap-southeast-1.amazonaws.com"
+HOST_GW = '' # Gateway IP for smarthive-gw.local.
 ROOT_CA = SNAP_COMMON + "/certs/root-CA.crt"
 PRIVATE_KEY = SNAP_COMMON + "/certs/CLC_PRIVATE.pem.key"
 CERT_FILE = SNAP_COMMON + "/certs/CLC_CERT.pem.crt"
@@ -31,9 +32,9 @@ LOCAL_PORT = 4545
 
 
 def mac_addr():
-    return (':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) 
-    for ele in range(0,8*6,8)][::-1]))  
-    
+    return (':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff)
+    for ele in range(0,8*6,8)][::-1]))
+
 # Configure logging
 logger = logging.getLogger("AWSIoTPythonSDK.core")
 logger.setLevel(logging.DEBUG)
@@ -51,31 +52,33 @@ def customCallback(client, userdata, message):
     logger.info(message.topic)
     payload = json.loads(message.payload)
     update_device_state(payload['hub_id'], payload['port'], payload['addr'], payload['state'])
-    logger.info("--------------\n\n")
 
 def update_device_state(hub_id, port, addr, state):
     try:
-     headers = {'Content-type': 'application/json', 'X-Dest-Nodes': hub_id, 'X-Auth-Token': 'SmartHive00'}
-     payload = {}
-     payload['command'] = "set_thing"
-     payload['port'] =  port
-     payload['addr'] = int(addr)
-     payload['state'] = int(state)
-     # curl -XPOST https://xxx.co '{"op":"set", "clientId":"60:f8:1d:ce:86:84", "hub_id":"30aea4e7e41c", "port":"K", "addr":0, "state":1}'
-     # curl -XPOST https://xxx.co '{"op":"set", "clientId":"60:f8:1d:ce:86:84", "hub_id":"30aea4e7e41c", "port":"K", "addr":0, "state":0}'
-     # Create http connection to GW
-     gwconnection = http.client.HTTPConnection("smarthive-gw.local", 80)
-     #gwconnection = http.client.HTTPConnection("192.168.1.22", 80)
-     gwconnection.set_debuglevel(10);
-     gwconnection.request("POST", "/comm", json.dumps(payload), headers)
-     response = gwconnection.getresponse()
-     data = response.read()
-     #logger.info(response.status, response.reason)
-     logger.info(data)
-     gwconnection.close()
+        headers = {'Content-type': 'application/json', 'X-Dest-Nodes': hub_id, 'X-Auth-Token': 'SmartHive00'}
+        payload = {}
+        payload['command'] = "set_thing"
+        payload['port'] =  port
+        payload['addr'] = int(addr)
+        payload['state'] = int(state)
+        # curl -XPOST https://xxx.co '{"op":"set", "clientId":"60:f8:1d:ce:86:84", "hub_id":"30aea4e7e41c", "port":"K", "addr":0, "state":1}'
+        # curl -XPOST https://xxx.co '{"op":"set", "clientId":"60:f8:1d:ce:86:84", "hub_id":"30aea4e7e41c", "port":"K", "addr":0, "state":0}'
+        # Create http connection to GW
+        entries = zeroconf.cache.entries_with_name('SmartHive-GW.local.');
+        if (len(entries) > 0):
+            HOST_GW = str(entries[0])
+            logger.info("Resolved SmartHive-GW IP: %s" % HOST_GW);
+            gwconnection = http.client.HTTPConnection(HOST_GW, 80)
+            gwconnection.request("POST", "/comm", json.dumps(payload), headers)
+            response = gwconnection.getresponse()
+            data = response.read()
+            logger.info(data)
+            gwconnection.close()
+        else:
+            logger.error('Could not resolve Gateway host');
     except Exception as e:
-     logger.error("HTTP Error while update device")
-     logger.error(e)
+        logger.error("HTTP Error while update device")
+        logger.error(e)
 
 # Init AWSIoTMQTTClient
 mqttClient = None
@@ -121,26 +124,24 @@ class CustomHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         return
 
-
+class mdnsListener:
+    def remove_service(self, zeroconf, type, name):
+        logger.info("Service removed: %s" % (name,))
+    def add_service(self, zeroconf, type, name):
+        info = zeroconf.get_service_info(type, name)
+        logger.info("Service added: %s, info: %s, ip: %s" % (name, info, zeroconf.cache.entries_with_name('SmartHive-GW.local.')))
 
 def start():
     global localHTTP, zeroconf, info, httpthread
     ip = get_local_address()
-    logging.info("Local IP is " + ip)
+    logging.info("Local IP: " + ip)
     desc = {"version": "0.1"}
-    info = ServiceInfo(
-        "_http._tcp.local.",
-        "SmartHive._http._tcp.local.",
-        socket.inet_aton(ip),
-        LOCAL_PORT,
-        0,
-        0,
-        desc,
-        LOCAL_HOST + ".",
-    )
+    info = ServiceInfo("_http._tcp.local.", "SmartHive-CLC._http._tcp.local.", socket.inet_aton(ip), LOCAL_PORT, 0, 0, desc, LOCAL_HOST + ".")
     zeroconf = Zeroconf()
+    listener = mdnsListener()
+    browser = ServiceBrowser(zeroconf, "_http._tcp.local.", listener)
     zeroconf.register_service(info)
-    logger.info("Local mDNS is started, domain is " + LOCAL_HOST)
+    logger.info("Local mDNS on domain: " + LOCAL_HOST)
     localHTTP = HTTPServer(("", LOCAL_PORT), CustomHandler)
     httpthread = threading.Thread(target=localHTTP.serve_forever)
     httpthread.start()
@@ -149,7 +150,6 @@ def mqtt_publish(message):
     messageJson = json.dumps(message)
     mqttClient.publish(TOPIC, messageJson, 1)
     logger.info("Published topic %s: %s\n" % (TOPIC, messageJson))
-
 
 def main():
     start()
